44f5d19081b97e4b97f32207b9724ce577526d54
StarringMaBark <mary.anna@barkley.info>
2026-04-02 19:35:06 -0500

    Time since animation played tracking

--- game/3D/scenes/RewindableAnimationPlayer.gd
+++ game/3D/scenes/RewindableAnimationPlayer.gd
@@ -16,26 +16,42 @@ class_name RewindableAnimationPlayer extends AnimationPlayer
 @export var track_name: String
 var reversed: bool = false
 
+var last_started: float
+var last_finished: float
+var animation_running: bool
+
 func _ready() -> void:
 	GlobalTime.rewind_all.connect(reverse)
 	GlobalTime.resume_all.connect(resume)
+	animation_started.connect(_start_animation)
 	animation_finished.connect(_done_reversing)
-	play(track_name)
+	if autoplay == "":
+		play(track_name)
 
 func reverse() -> void:
 	if reversed:
 		return
 	GlobalTime.num_rewinding += 1
 	reversed = true
-	play_section_backwards(track_name, 0.0, current_animation_position)
+	if animation_running:
+		play_section_backwards(track_name, 0.0, current_animation_position)
+	else:
+		var delta_time: float = Time.get_unix_time_from_system() - last_finished
+		get_tree().create_timer(delta_time).timeout.connect(func(): play_section_backwards(track_name, 0.0, current_animation_length))
 
 func resume() -> void:
 	if !reversed:
 		return
 	GlobalTime.num_rewinding -= 1
 	reversed = false
-	##TODO: this should only replay tracks that are automatically started (not a door opening)
-	play_section(track_name, current_animation_position)
-	
+	if autoplay != "":
+		play_section(track_name, current_animation_position)
+
+func _start_animation(_anim_name: StringName) -> void:
+	last_started = Time.get_unix_time_from_system()
+	animation_running = true;
+
 func _done_reversing(_anim_name: StringName) -> void:
 	resume()
+	last_finished = Time.get_unix_time_from_system()
+	animation_running = false;
